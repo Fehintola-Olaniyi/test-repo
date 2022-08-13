60699fb9458d2a062de54360ad0ffb89ce2a6a99
Fehintola-Olaniyi <107301789+Fehintola-Olaniyi@users.noreply.github.com>
2022-08-13 02:41:09 -0700

    modified coinChanger

--- coinChanger.js
+++ coinChanger.js
@@ -1,4 +1,4 @@
-
+const prompt = require("prompt-sync")();
 console.log("Change Calculator");
 function coinChanger(centVal) {
   const coinType = {
@@ -11,34 +11,34 @@ function coinChanger(centVal) {
 let quart = coinType.Quarters, dime = coinType.Dimes, nick = coinType.Nickels, pen = coinType.Pennies;
 
   if (centVal >= 0 && centVal <= 99) {
-     var a = Math.floor(centVal / quart);
+     let a = Math.floor(centVal / quart);
          console.log("Quarters: " + a);
   }
   if (centVal % quart >= 10) {
-     var b = centVal % quart;
-     var c = Math.floor(b / dime);
+     let b = centVal % quart;
+     let c = Math.floor(b / dime);
         console.log("Dimes: " + c);
   }
    else if (centVal % quart >= 5) {
          console.log("Dimes: 0");
-     var g = Math.floor((centVal % quart) / nick);
+     let g = Math.floor((centVal % quart) / nick);
         console.log(`Nickels: ${g}`);
         console.log("pennies: " + ((centVal % quart) % nick));
   }
   if (centVal % quart < 5) {
-     var h = centVal % quart;
+     let h = centVal % quart;
         console.log("Dimes: 0\nNickels: 0\nPennies: " + h);
   }
   if (b % dime >= 5) {
-     var d = b % dime;
-     var e = Math.floor(d / nick);
+     let d = b % dime;
+     let e = Math.floor(d / nick);
         console.log("Nickels: " + e);
   }
    else if (b % dime < 5) {
         console.log("Nickels: 0\nPennies: " + (b % dime));
   }
   if (d % nick >= 1) {
-     var f = d % nick;
+     let f = d % nick;
         console.log("Pennies: " + f);
   }
    else if (d % nick == 0) {
@@ -47,7 +47,7 @@ let quart = coinType.Quarters, dime = coinType.Dimes, nick = coinType.Nickels, p
 }
 
 function mainGuy() {
-  const prompt = require("prompt-sync")();
+  
   let cent = prompt("Enter number of cents (0-99): ");
     coinChanger(cent);
 
