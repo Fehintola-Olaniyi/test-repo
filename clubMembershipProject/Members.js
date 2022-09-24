46276d669ac281691881cc342422c532c36f57ee
Fehintola-Olaniyi <107301789+Fehintola-Olaniyi@users.noreply.github.com>
2022-09-24 14:24:03 +0100

    Included lists for staff and student

--- clubMembershipProject/Members.js
+++ clubMembershipProject/Members.js
@@ -1,74 +1,112 @@
 import { Member } from './Main.js';
-const prompt = require('prompt-sync')();
 class Student extends Member {
-  constructor(first, last, gender, dateOfBirth, phoneNumber, emailAddress, studentNo, cla_ss) {
-    super(first, last, gender, dateOfBirth, phoneNumber, emailAddress);
-    this.cla_ss = cla_ss;
-    this.studentNo = studentNo;
+  // constructor(first, last, gender, dateOfBirth, phoneNumber, emailAddress, studentNo, cla_ss) {
+    // super(first, last, gender, dateOfBirth, phoneNumber, emailAddress);
+    rank = "student";
+    cla_ss = cla_ss;
+    studentNo = studentNo;
   }
-}
+// }
 
 class Staff extends Member {
-  constructor(first, last, gender, dateOfBirth, phoneNumber, emailAddress) {
-    super(first, last, gender, dateOfBirth, phoneNumber, emailAddress);
-    this.status = "Staff";
-    this.staffNo = 0;
-    this.netPay = 2000;
+  // constructor(first, last, gender, dateOfBirth, phoneNumber, emailAddress) {
+    // super(first, last, gender, dateOfBirth, phoneNumber, emailAddress);
+    rank = "Staff";
+    staffNo = 0;
+    netPay = 2000;
  
-  // assignToStudent(student_number) {
+  assignToStudent(student_number) {
     
   }
 } 
 
-let dom = new Staff("john", "doe", "f", 1999, 3928734, "isdn@gmail.com");
-console.log(dom.gender);
-console.log(dom.dateOfRegistration);
-// console.log(dom.emailAddress);
-// console.log(dom.phoneNumber)
-console.log(dom.netPay);
+let jane = new Staff("Jane", "Doe", "f", 1999, 39282, "janedoe@gmail.com");
+let james = new Staff("James", "Keys", "m", 2000, 37891, "jameskeys@gmail.com");
+let matthew = new Staff("Matthew", "Jill", "m", 2004, 29373, "mattjill@gmail.com");
+let alicia = new Staff("Alicia", "Morgan", "f", 1982, 90372, "alimorgan@gmail.com");
 
 
+class List {
+  listSize = 0;
+  pos = 0;
+  dataStore = [];
+  insert;
 
+  merge(element) {
+    this.dataStore[this.listSize++] = element;
+  }
+  finds(element) {
+    for(let i = 0; i < this.dataStore.length; i++) {
+      if(this.dataStore[i] == element) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  removes(element) {
+    let foundAt = this.finds(element);
+    if(foundAt > -1) {
+      this.dataStore.splice(foundAt, 1);
+      this.listSize--;
+      return true;
+    }
+    return false;
+  }
+  length() {
+    return this.listSize;
+  }
+  showStore() {
+    return this.dataStore;
+  }
+  clears() {
+    delete this.dataStore;
+    this.dataStore = [];
+    this.listSize = this.pos = 0;
+  }
+  contains(element) {
+    for(let i = 0; i < this.dataStore.length; i++) {
+      if(this.dataStore[i] == element) {
+        return true;
+      }
+    }
+    return false;
+  }
+  front() {  //advantages of using iterators over array indexing...
+    this.pos = 0;
+  }
+  end() {
+    this.pos = this.listSize-1;
+  }
+  prev() {
+    if(this.pos > 0) {
+      this.pos--;
+    }
+  }
+  next() {
+    if(this.pos < this.listSize-1) {
+      this.pos++
+    }
+  }
+  currPos() {
+    return this.pos;
+  }
+  moveto(position) {
+    this.pos = position;
+  }
+  getElement() {
+    return  this.dataStore[this.pos];
+  }
 
+}
+let staffList = new List();
+let studentList = new List();
+staffList.merge(jane);
+staffList.merge(james);
+staffList.merge(matthew);
+staffList.merge(alicia);
+console.log(staffList.showStore());
+// staffList.front();
+// console.log(staffList.getElement());
+// staffList.next();
+// console.log(staffList.getElement());
 
-
-
-
-// let rankPr = prompt("Are you registering as a Student or Staff?: ");
-// if (rankPr == "student") {
-//   userInput();
-//   let studentNoPr = prompt("input your student number: ");
-//   let classPr = prompt("Input your current class: ");
-//   let potentialStudent = new Student(
-//     fNamePr,
-//     lNamePr,
-//     gendPr,
-//     dobPr,
-//     phonePr,
-//     emailPr,
-//     studentNoPr,
-//     classPr
-//   );
-// } else {
-//   // let staffNoPr = prompt("Input your staff number: ");
-//   // let netPayPr = prompt("Input your net pay: ");
-//   let potentialStaff = new Staff(
-//     fNamePr,
-//     lNamePr,
-//     gendPr,
-//     dobPr,
-//     phonePr,
-//     emailPr,
-//     staffNoPr,
-//     netPayPr
-//   );
-// }
-
-// function userInput() {
-//   let fNamePr = prompt("Input your  first name: ");
-//   let lNamePr = prompt("Input your last name: ");
-//   let gendPr = prompt("Input your gender(m/f): ");
-//   let dobPr = prompt("Input your date of birth: ");
-//   let phonePr = prompt("Input your phone number: ");
-//   let emailPr = prompt("Inout your email address: ");
-// }
